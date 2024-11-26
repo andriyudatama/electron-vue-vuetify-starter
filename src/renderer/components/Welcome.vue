50a8959d1297215af3c7c82eeaa674e60faf8190
Andri Yudatama <andriecool@gmail.com>
2024-11-26 18:43:06 +0700

    Create welcome component

---
+++ src/renderer/components/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-responsive class="fill-height justify-center align-center">
+    <v-container class="d-flex justify-center">
+      <v-img src="assets/vue.svg" max-width="150" class="mr-8"></v-img>
+      <v-img src="assets/vite.svg" max-width="150" class="mr-8"></v-img>
+      <!-- <v-img src="assets/vuetify.svg" max-width="150"></v-img> -->
+    </v-container>
+    <v-container class="d-flex flex-column text-center justify-center">
+      <div class="text-h4 font-weight-bold">
+        Electron + Vue + Vuetify Starter
+      </div>
+      <v-card class="mt-4">
+        <v-card-text>
+          <div>
+            This is a starter template powered with Vue and Vuetify for Electron projects. <br>
+            This starter template is preinstalled with Vue Router.
+          </div>
+          <div class="">
+            <div class="text-overline text-uppercase">Development</div>
+            <pre>yarn dev</pre>
+            <div class="text-overline text-uppercase">Build</div>
+            <pre>yarn build</pre>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-responsive>
+</template>
--- src/renderer/pages/Home.vue
+++ src/renderer/pages/Home.vue
@@ -1,26 +1,11 @@
 <script setup>
 import { ref } from 'vue'
+import Welcome from '../components/Welcome.vue'
 const count = ref(0)
 </script>
 
 <template>
-  <v-responsive class="fill-height justify-center align-center">
-    <v-row class="d-flex justify-center flex-wrap">
-      <v-col cols="4">
-        <v-card>
-          <v-card-title>Congratulations</v-card-title>
-          <v-img src="https://picsum.photos/400/300"></v-img>
-          <v-card-text>
-            You have already installed Electron + Vue + Vuetify. This is a template to build desktop app quickly and easily.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="success" @click="count++">Click ({{ count }})</v-btn>
-            <v-btn color="error">Cancel</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-responsive>
+  <Welcome></Welcome>
 </template>
 
 <style scoped>
